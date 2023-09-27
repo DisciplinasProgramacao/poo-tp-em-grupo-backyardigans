ea47d21ceb2d991b1eb25e5066a34b005f36898a
Ana Clara <1233055@sga.pucminas.br>
2023-09-27 17:21:46 -0300

    Cria a classe Rota, de acordo com o diagrama.

---
+++ codigo/Rota.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+//*Cria a classe Rota, com dois atributos privados, quilometragem, em double, e data, em data. */
+public class Rota{
+    private double quilometragem;
+    private Date data;
+
+/*Cria um objeto da classe Rota, que recebe como parâmetro a quilometragem em double e a data em date. */
+    public Rota (double quilometragem, Date data){
+        this.quilometragem=quilometragem;
+        this.data=data;
+    }
+
+    public String relatorio(){
+        
+    }
+
+}
