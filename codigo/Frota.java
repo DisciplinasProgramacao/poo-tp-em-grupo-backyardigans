9c21fa45e498cd9de4a64b33d8f65e9224d35dd1
CamilleI <camille.goncalves@sga.pucminas.br>
2023-09-28 10:48:56 -0300

    Inicio da classe frota e atributos da classe veiculo

---
+++ codigo/Frota.java
@@ -0,0 +1,92 @@
+public class Frota {
+    
+    private int tamanhoFrota;
+    private Veiculo veiculos[];
+
+    public Frota(int tamanhoFrota, Veiculo[] veiculos) {
+        this.tamanhoFrota = tamanhoFrota;
+        this.veiculos = new Veiculo [50]; //verificar se tem algum número limite base ou se pode usar estrutura de dados
+    }
+
+    /**
+     * Metódo que retorna uma String com um relatório da frota de veículos. 
+     * Ele é composto pela quantidade de veículos presente na frota e pelos veículos existentes.
+     * @return String segundo a descrição:
+     * <ul> <li><li> <ul>
+     */
+    public String toString(){
+        StringBuilder relatorio = new StringBuilder();
+
+        relatorio.append("RELATÓRIO DA FROTA DE PEDIDOS\n");
+        relatorio.append("-----------------------------\n");
+
+        if(tamanhoFrota == 0){
+            relatorio.append("Frota vazia");
+        }else{
+            relatorio.append("Tamanho da frota: "+tamanhoFrota);
+        }
+
+        for(int i =0; i < veiculos.length; i++){
+            relatorio.append(String.format("02d", i));
+            relatorio.append(veiculos.toString()+"\\n");
+        }
+
+        return relatorio.toString();
+    }
+
+    /**
+     * Metódo que vai localizar, a partir da placa fornecida, se o veículo existe na frota
+     * @param placa Placa do veículo que será pesquisado
+     * @return Veículo procurado
+     */
+    public Veiculo localizarVeiculo(String placa){
+        Veiculo veiculoLocalizado = null;
+
+        for(Veiculo x : veiculos){
+            if(placa.equalsIgnoreCase(x.getPlaca())){
+                veiculoLocalizado = x;
+            }
+        }
+
+        return veiculoLocalizado;
+    }
+
+    /**
+     * Metódo que vai retornar a quilometragem total de todos os veículos da frota
+     */
+    public double quilometragemTotal(){
+        double totalQuilometragem = 0;
+
+        for(Veiculo x : veiculos){
+            totalQuilometragem += x.kmTotal();
+        }
+
+        return totalQuilometragem;
+    }
+
+    /**
+     * Metódo que vai retornar o veículo com a maior quilometragem da frota
+     * @return Veículo com a maior quilometragem da frota
+     */
+    public Veiculo maiorKmTotal(){
+        double maiorKm = 0;
+        Veiculo veiculoMaiorKm = null;
+
+        for(Veiculo x : veiculos){
+            if(x.kmTotal() > maiorKm){
+                maiorKm = x.kmTotal();
+                veiculoMaiorKm = x;
+            }
+        }
+
+        return veiculoMaiorKm;
+    }
+
+    /**
+     * Metódo que vai retornar o veículo com a maior quilometragem média da frota
+     * @return Veículo com a maior quilometragem média da frota.
+     */
+    public Veiculo maiorKmMedia(){
+        return null;
+    }
+}
---
+++ codigo/Veiculo.java
@@ -0,0 +1,13 @@
+public class Veiculo {
+
+    private String placa;
+    private Rota rotas[];
+    private int quantRotas;
+    private double tanqueAtual;
+    private double tanqueMaximo;
+    private double totalReabastecido;
+
+    public String getPlaca() {
+        return this.placa;
+    }
+}
