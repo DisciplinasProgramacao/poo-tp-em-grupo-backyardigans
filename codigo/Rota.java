941f360929128c14a75ce7c1078f410a47a0b755
GustavoSilvino <gustavo.silvino@sga.pucminas.br>
2023-10-01 21:04:57 -0300

    criação da classe Tanque

--- codigo/Rota.java
+++ codigo/Rota.java
@@ -12,7 +12,7 @@ public class Rota{
     }
 
     public String relatorio(){
-        
+        return "";
     }
 
 }
--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,15 +1,16 @@
 public class Veiculo {
 
     private static final int MAX_ROTAS;
+    private static final double CONSUMO;
     private String placa;
     private Rota rotas[];
     private int quantRotas;
-    private double tanqueAtual;
-    private double tanqueMaximo;
+    private Tanque tanque;
     private double totalReabastecido;
 
     static{
         MAX_ROTAS = 30;
+        CONSUMO = 8.2;
     }
 
     public String getPlaca() {
@@ -24,5 +25,15 @@ public class Veiculo {
         double kmTotal =0;
 
         return kmTotal;
+    }
+
+    private double autonomiaAtual() {
+        double quantidadeTanque = tanque.autonomiaAtual();
+        return quantidadeTanque;
+    }
+
+    private double autonomiaMaxima() {
+        double quantidadeTanque = tanque.autonomiaMaxima();
+        return quantidadeTanque;
     }
 }
