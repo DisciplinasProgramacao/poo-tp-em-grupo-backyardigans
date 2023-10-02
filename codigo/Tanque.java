0e8ea7f5c6a0c5abfecec19521cd2b1f17b32c3e
GustavoSilvino <gustavo.silvino@sga.pucminas.br>
2023-10-01 21:12:05 -0300

    criação da classe Tanque

---
+++ codigo/Tanque.java
@@ -0,0 +1,55 @@
+public class Tanque {
+    private static final double CONSUMO;
+    private double capacidadeMaxima;
+    private double capacidadeAtual;
+
+
+    static{
+        CONSUMO = 8.2;
+    }
+
+    public Tanque(double cMaxima) {
+        this.capacidadeMaxima = cMaxima;
+        this.capacidadeAtual = capacidadeMaxima;
+    }
+    
+    /**
+     * Abastace o tanque do carro de acordo com a quantidade fornecida, ignorando o sinal do valor
+     * @param litros Litros a serem abastecidos dentro do tanque
+     * @return a capacidade atual do tanque depois de abastecido, em Double
+     */
+    public double abastecer(double litros) {
+
+        if(litros < 0) {
+            litros = litros * (-1);
+        }
+
+        if(capacidadeAtual + litros < capacidadeMaxima){
+            capacidadeAtual += litros;
+        } else if(capacidadeAtual + litros >= capacidadeMaxima){
+            capacidadeAtual = capacidadeMaxima;
+        }
+        return capacidadeAtual;
+    }
+
+    /**
+     * Fornece quilometragem possivel de acordo com a capacidade maxima do tanque
+     * @return o valor em double da capacidade maxima do tanque vezes o consumo do veiculo
+     */
+
+    public double autonomiaMaxima() {
+
+        double kmPossiveis = capacidadeMaxima * CONSUMO;
+        return kmPossiveis;
+    }
+
+    /**
+     * Fornece quilometragem possivel da capacidade Atual do tanque
+     * @return o valor em double da capacidade atual do tanque vezes o consumo do veiculo
+     */
+    public double autonomiaAtual() {
+        
+        double kmPossiveis = capacidadeAtual * CONSUMO;
+        return kmPossiveis;
+    }
+}
