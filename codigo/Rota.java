3d693c32c009c467c8ee2c25c4045205ec88a443
Ana Clara <1233055@sga.pucminas.br>
2023-10-15 15:16:07 -0300

    Altera a classe rota

--- codigo/Rota.java
+++ codigo/Rota.java
@@ -1,18 +1,32 @@
 import java.util.*;
 
-//*Cria a classe Rota, com dois atributos privados, quilometragem, em double, e data, em data. */
 public class Rota{
     private double quilometragem;
     private Date data;
 
-/*Cria um objeto da classe Rota, que recebe como parâmetro a quilometragem em double e a data em date. */
+/**
+ * Cria um objeto da classe Rota.
+ * @param quilometragem quilometragem da rota, do tipo double
+ * @param data data da rota, do tipo date
+ */
     public Rota (double quilometragem, Date data){
         this.quilometragem=quilometragem;
         this.data=data;
     }
 
-    public String relatorio(){
-        return "";
-    }
+/**
+ *Relatório da rota. Formatação em 2 linhas, com <ul>
+     * <li> Data da rota </li>
+     * <li> Quilometragem da rota </li>
+     * <ul>
+     * @return Uma string contendo o relatório da rota, conforme informações acima. 
+ */
+ @Override
+	public String toString() {
 
+		StringBuilder aux = new StringBuilder();
+        aux.append(" DATA: "+data+"\n");
+        aux.append(" QUILOMETRAGEM: "+quilometragem+ " km");
+        return aux.toString();
+	}
 }
